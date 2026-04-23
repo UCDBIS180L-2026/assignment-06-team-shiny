3b7fcad45959c9fb96ea4112a708eaee535c13f8
DaleDeng <rxdeng@ucdavis.edu>
2026-04-23 15:19:33 -0700

    Serverpart team project

--- teamApp/app.R
+++ teamApp/app.R
@@ -1,6 +1,6 @@
 library(shiny)
 # other libraries here
-
+library(tidyverse)
 # data loading and one-time processing here
 
 
@@ -13,6 +13,19 @@ ui <- fluidPage( #create the overall page
 # Define server logic 
 server <- function(input, output) {
   # server code here
+  output$plot <- renderPlot({
+    
+    sleepStats <- as.name(input$sleep_stats) #convert as variable names
+    
+    msleep %>% #select from input:genus for filtering
+      filter(vore != input$genus) %>%
+      ggplot(aes(x = conservation, 
+                 y = !! sleepStats,
+                 fill = conservation)) + #then plot the sleep stats with conservation
+      geom_boxplot(alpha=0.75) +
+      labs(x= "Conservation", fill="Conservation Status") +
+      theme_minimal()
+  })
 }
 
 # Run the application 
